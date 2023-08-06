3a13de5509c6c5e3e1fce413e3e6f454b25432e1
chris3evans <chris3evans.ce@gmail.com>
2023-08-06 10:52:31 +0100

    [Lesson 8]

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,3 +1,10 @@
 <?php
+  function generateText($name, $colour) {
+    echo "<p>Hi, my name is $name and my favourite colour is $colour. </p>";
+  }
 
+  generateText('John', 'blue');
+  generateText('Jane', 'green');
 ?>
+
+<h1><?php bloginfo('name');?></h1>
