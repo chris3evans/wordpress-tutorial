946c060e1b6c7c50993586d4fd844b373a977503
chris3evans <chris3evans.ce@gmail.com>
2023-08-07 17:53:49 +0100

    [Lesson 15]

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -7,4 +7,9 @@
     wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
   }
   add_action('wp_enqueue_scripts', 'university_files');
+
+  function university_features() {
+    add_theme_support('title-tag');
+  }
+  add_action('after_setup_theme', 'university_features');
 ?>
