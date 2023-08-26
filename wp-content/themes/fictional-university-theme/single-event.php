ea75f33cd0817b8a7ec8d1e6f2e0fbc6a9f32d1b
chris3evans <chris3evans.ce@gmail.com>
2023-08-26 11:49:34 +0100

    [Lesson 37]

--- wp-content/themes/fictional-university-theme/single-event.php
+++ wp-content/themes/fictional-university-theme/single-event.php
@@ -26,6 +26,22 @@
     <div class="generic-content">
       <?php the_content();?>
     </div>
+    <?php
+      $relatedPrograms = get_field('related_program');
+
+      if ($relatedPrograms) {
+        echo "<hr class='section-break'>";
+        echo "<h2 class='headline headline--medium'>Related Program(s)</h2>";
+        echo "<ul class='link-list min-list'>";
+        foreach($relatedPrograms as $relatedProgram) {
+    ?>
+        <li>
+          <a href="<?php echo get_the_permalink($relatedProgram);?>"><?php echo get_the_title($relatedProgram);?></a>
+        </li>
+    <?php }
+        echo "</ul>";
+      }
+    ?>
   </div>
 <?php
   }
