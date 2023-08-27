ba7ebd15464b46c58ccb3fae930aabeb14e2ca70
chris3evans <chris3evans.ce@gmail.com>
2023-08-27 19:52:13 +0100

    [Lesson 45]

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -3,16 +3,12 @@
 <?php
   while(have_posts()) {
     the_post();
+    pageBanner([
+      'title' => 'Hello There',
+      'subtitle' => 'Hi, this is the subtitle',
+      'photo' => 'https://images.unsplash.com/photo-1493246507139-91e8fad9978e?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop='
+    ]);
 ?>
-<div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg');?>)"></div>
-  <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title"><?php the_title();?></h1>
-    <div class="page-banner__intro">
-      <p>DON'T FORGET TO REPLACE ME LATER</p>
-    </div>
-  </div>
-</div>
 
 <div class="container container--narrow page-section">
   <?php
@@ -20,15 +16,15 @@
     $parent_page_id = wp_get_post_parent_id($current_page_id);
 
     if ($parent_page_id) { ?>
-            <div class="metabox metabox--position-up metabox--with-home-link">
-              <p>
-                <a class="metabox__blog-home-link" href="<?php echo get_permalink($parent_page_id);?>">
-                  <i class="fa fa-home" aria-hidden="true"></i>
-                  Back to <?php echo get_the_title($parent_page_id);?>
-                </a>
-                <span class="metabox__main"><?php the_title();?></span>
-              </p>
-            </div>
+      <div class="metabox metabox--position-up metabox--with-home-link">
+        <p>
+          <a class="metabox__blog-home-link" href="<?php echo get_permalink($parent_page_id);?>">
+            <i class="fa fa-home" aria-hidden="true"></i>
+            Back to <?php echo get_the_title($parent_page_id);?>
+          </a>
+          <span class="metabox__main"><?php the_title();?></span>
+        </p>
+      </div>
     <?php } ?>
 
   <?php
