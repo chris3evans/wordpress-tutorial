b5e407f06e487db1f09c3b5c950d15035ea7c14a
chris3evans <chris3evans.ce@gmail.com>
2023-08-28 11:55:18 +0100

    [Lesson 48]

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -42,10 +42,10 @@
   add_action('pre_get_posts', 'university_adjust_queries');
 
   function pageBanner($args = NULL) {
-    if (!$args['title']) $args['title'] = get_the_title();
-    if (!$args['subtitle']) $args['subtitle'] = get_field('page_banner_subtitle');
-    if (!$args['photo']) {
-      if (get_field('page_banner_background_image')) {
+    if (!isset($args['title'])) $args['title'] = get_the_title();
+    if (!isset($args['subtitle'])) $args['subtitle'] = get_field('page_banner_subtitle');
+    if (!isset($args['photo'])) {
+      if (get_field('page_banner_background_image') AND !is_archive() AND !is_home()) {
         $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
       } else {
         $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
