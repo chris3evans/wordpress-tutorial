e47bbd0614019fc1b61ff83bfe0bb15a6ebe62f0
chris3evans <chris3evans.ce@gmail.com>
2023-09-04 18:41:24 +0100

    [Lesson 82]

--- wp-content/themes/fictional-university-theme/inc/search-route.php
+++ wp-content/themes/fictional-university-theme/inc/search-route.php
@@ -40,6 +40,17 @@
       }
 
       if (get_post_type() === 'program') {
+        $relatedCampuses = get_field('related_campus');
+
+        if ($relatedCampuses) {
+          foreach ($relatedCampuses as $campus) {
+            array_push($results['campuses'], [
+              'title' => get_the_title($campus),
+              'permalink' => get_the_permalink($campus)
+            ]);
+          }
+        }
+
         array_push($results['programs'], [
           'title' => get_the_title(),
           'permalink' => get_the_permalink(),
@@ -88,12 +99,31 @@
       }
 
       $programRelationshipQuery = new WP_Query([
-        'post_type' => 'professor',
+        'post_type' => ['professor', 'event'],
         'meta_query' => $programsMetaQuery
       ]);
 
       while ($programRelationshipQuery -> have_posts()) {
         $programRelationshipQuery -> the_post();
+
+        if (get_post_type() === 'event') {
+          $eventDate = new DateTime(get_field('event_date'));
+          $description = null;
+
+          if (has_excerpt()) {
+            $description = get_the_excerpt();
+          } else {
+            $description =  wp_trim_words(get_the_content(), 18);
+          }
+
+          array_push($results['events'], [
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink(),
+            'month' => $eventDate -> format('M'),
+            'day' => $eventDate -> format('d'),
+            'description' => $description,
+          ]);
+        }
 
         if (get_post_type() === 'professor') {
           array_push($results['professors'], [
@@ -105,6 +135,7 @@
       }
 
       $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+      $results['events'] = array_values(array_unique($results['events'], SORT_REGULAR));
     }
 
     return $results;
