752c4bfe4ee6ce0ccc4e36de607148ee35917ace
chris3evans <chris3evans.ce@gmail.com>
2023-09-05 18:19:56 +0100

    [Lesson 84]

---
+++ wp-content/themes/fictional-university-theme/page-search.php
@@ -0,0 +1,74 @@
+<?php get_header();?>
+
+<?php
+  while(have_posts()) {
+    the_post();
+    pageBanner([
+      'title' => 'Search',
+      'subtitle' => '',
+      'photo' => 'https://images.unsplash.com/photo-1493246507139-91e8fad9978e?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop='
+    ]);
+?>
+
+<div class="container container--narrow page-section">
+  <?php
+    $current_page_id = get_the_ID();
+    $parent_page_id = wp_get_post_parent_id($current_page_id);
+
+    if ($parent_page_id) { ?>
+      <div class="metabox metabox--position-up metabox--with-home-link">
+        <p>
+          <a class="metabox__blog-home-link" href="<?php echo get_permalink($parent_page_id);?>">
+            <i class="fa fa-home" aria-hidden="true"></i>
+            Back to <?php echo get_the_title($parent_page_id);?>
+          </a>
+          <span class="metabox__main"><?php the_title();?></span>
+        </p>
+      </div>
+    <?php } ?>
+
+  <?php
+    $currentPages = ['child_of' => get_the_ID()];
+    $isParentPage = get_pages($currentPages);
+    if ($parent_page_id || $isParentPage) {
+  ?>
+  <div class="page-links">
+    <h2 class="page-links__title">
+      <a href="<?php echo get_permalink($parent_page_id);?>"><?php echo get_the_title($parent_page_id);?></a>
+    </h2>
+
+    <ul class="min-list">
+      <?php
+        if ($parent_page_id) {
+          $findChildrenOf = $parent_page_id;
+        } else {
+          $findChildrenOf = $current_page_id;
+        }
+
+        $children_list_array = [
+          'title_li' => null,
+          'child_of' => $findChildrenOf,
+          'sort_column' => 'menu_order'
+        ];
+        wp_list_pages($children_list_array);
+      ?>
+    </ul>
+  </div>
+  <?php } ?>
+
+  <div class="generic-content">
+    <!-- GET makes sure content of form is added to URL -->
+    <form class="search-form" method="get" action="<?php echo esc_url(site_url('/'));?>">
+      <label class="headline headline--medium" for="s">Perform a New Search</label>
+      <div class="search-form-row">
+        <input class="s" type="search" name="s" id="s" placeholder="What are you looking for?"/>
+        <input class="search-submit" type="submit" value="Search"/>
+      </div>
+    </form>
+  </div>
+</div>
+<?php
+  }
+?>
+
+<?php get_footer();?>
