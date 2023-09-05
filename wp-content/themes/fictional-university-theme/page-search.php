6d737f3595062985a9c042034e2034a7b62dddd2
chris3evans <chris3evans.ce@gmail.com>
2023-09-05 22:03:48 +0100

    [Lesson 85]

--- wp-content/themes/fictional-university-theme/page-search.php
+++ wp-content/themes/fictional-university-theme/page-search.php
@@ -58,13 +58,7 @@
 
   <div class="generic-content">
     <!-- GET makes sure content of form is added to URL -->
-    <form class="search-form" method="get" action="<?php echo esc_url(site_url('/'));?>">
-      <label class="headline headline--medium" for="s">Perform a New Search</label>
-      <div class="search-form-row">
-        <input class="s" type="search" name="s" id="s" placeholder="What are you looking for?"/>
-        <input class="search-submit" type="submit" value="Search"/>
-      </div>
-    </form>
+    <?php get_search_form();?>
   </div>
 </div>
 <?php
