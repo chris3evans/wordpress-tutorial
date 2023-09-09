d433b946f24f7be8f055483aad4f30c0a34ce278
chris3evans <chris3evans.ce@gmail.com>
2023-09-09 14:58:44 +0100

    [Lesson 98]

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -124,6 +124,11 @@
   }
   add_filter('login_headertitle', 'ourLoginTitle');
   function makeNotePrivate ($data) {
+    if ($data['post_type'] === 'note') {
+      $data['post_content'] = sanitize_textarea_field($data['post_content']);
+      $data['post_title'] = sanitize_text_field($data['post_title']);
+    }
+
     if ($data['post_type'] === 'note' AND $data['post_status'] !== 'trash') {
       $data['post_status'] = 'private';
     }
